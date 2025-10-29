82e945abbfde262826d14ea20b5a11cb69c275ac
Muhammad Ali Fajri <muhammadalifajri91@gmail.com>
2025-10-30 05:29:40 +0700

    Update main.jsx ke2

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import FlipFlopPage from './pages/FlipFlopPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/flipflop/:type" element={<FlipFlopPage />} />
         <Route path="/flipflop" element={<FlipFlopPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
