17ef8faaecb791fa9357f48f8871a4e260cc37b9
Muhammad Ali Fajri <muhammadalifajri91@gmail.com>
2025-10-30 05:42:33 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import FlipFlopPage from './pages/FlipFlopPage'
-import NotFoundPage from './pages/NotFoundPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/flipflop/:type" element={<FlipFlopPage />} />
         <Route path="/flipflop" element={<FlipFlopPage />} />
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
