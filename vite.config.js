84e977eff3a246ec367404ddc088ed2351572113
Muhammad Ali Fajri <muhammadalifajri91@gmail.com>
2025-10-30 05:41:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,10 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  publicDir: 'public',
 })
